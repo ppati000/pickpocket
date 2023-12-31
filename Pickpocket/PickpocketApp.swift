2caedc08752d088a68489b19baa806841e306f7b
Patrick Petrovic <ppati000@me.com>
2023-12-31 12:24:20 +0100

    Initial Commit

---
+++ Pickpocket/PickpocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickpocketApp.swift
+//  Pickpocket
+//
+//  Created by Patrick Petrovic on 31.12.23.
+//
+
+import SwiftUI
+
+@main
+struct PickpocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
